09b1079ed86e98fd99f569c1c6977fa5ad38fa3f
Alonso Cesar Terrazas Salas <alonso.terrazas@coppel.com>
2024-07-25 17:05:26 -0700

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { colIconsClose, colIconsMenu } from 'col-library-example';
+import {NgColLibraryExampleModule, NgColLibraryExampleService} from 'ng-col-library-example';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgColLibraryExampleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'col-library-test';
+
+  constructor(private colService:NgColLibraryExampleService){
+    colService.registerIcons([colIconsClose,colIconsMenu]);
+    
+  }
 }
